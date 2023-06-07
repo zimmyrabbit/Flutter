46753b4a0eb90166df0062704d664b26d526dde6
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-06-07 23:37:14 +0900

    PageController

--- image_carousel/lib/screen/home_screen.dart
+++ image_carousel/lib/screen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,21 +11,36 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   Timer? timer;
+  PageController controller = PageController(
+    initialPage: 0,
+  );
 
   @override
   void initState() {
     super.initState();
 
-    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+    timer = Timer.periodic(Duration(seconds: 4), (timer) {
       print('Timer');
+      int currentPage = controller.page!.toInt();
+      int nextPage = currentPage + 1;
+
+      if (nextPage > 4) {
+        nextPage = 0;
+      }
+
+      controller.animateToPage(
+          nextPage,
+          duration: Duration(milliseconds: 400),
+          curve: Curves.linear
+      );
     });
   }
 
   @override
   void dispose() {
-    if(timer != null) {
+    controller.dispose();
+    if (timer != null) {
       timer!.cancel();
     }
 
@@ -33,8 +49,10 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
     return Scaffold(
       body: PageView(
+        controller: controller,
         children: [1, 2, 3, 4, 5]
             .map(
               (e) => Image.asset(
