87230abe75c95c86b088b2ed0d63f1cb79e6b726
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-06-07 22:35:15 +0900

    statefulWidget 선언

---
+++ statefulwidget/lib/screen/home_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+//StatefulWidget
+class HomeScreen extends StatefulWidget {
+  final Color color;
+
+  const HomeScreen({
+    required this.color,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeScreenState();
+  }
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50.0,
+      height: 50.0,
+      color: widget.color,
+    );
+  }
+}
+
+//StatelessWidget
+class _HomeScreen extends StatelessWidget {
+  final Color color;
+
+  const _HomeScreen({
+    required this.color,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50.0,
+      height: 50.0,
+      color: color,
+    );
+  }
+}
