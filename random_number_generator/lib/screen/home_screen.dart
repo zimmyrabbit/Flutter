c7424b36aadfa493f04d0108aebc7f5f29fbd2d2
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-06-11 17:42:35 +0900

    map function 으로 숫자별 이미지 매핑

--- random_number_generator/lib/screen/home_screen.dart
+++ random_number_generator/lib/screen/home_screen.dart
@@ -48,14 +48,29 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
                 Expanded(
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('123'),
-                      Text('456'),
-                      Text('789'),
-                    ],
-                  ),
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        123,
+                        456,
+                        789,
+                      ]
+                          .map(
+                            (x) => Row(
+                              children: x
+                                  .toString()
+                                  .split('')
+                                  .map(
+                                    (y) => Image.asset(
+                                      'asset/img/$y.png',
+                                      height: 70.0,
+                                      width: 50.0,
+                                    ),
+                                  )
+                                  .toList(),
+                            ),
+                          )
+                          .toList()),
                 ),
                 SizedBox(
                   width: double.infinity,
