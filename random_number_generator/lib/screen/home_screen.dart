c091200b03cacd61b0b695a07c00a4bd7a5a38de
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-06-11 17:27:01 +0900

    디자인 적용하기

--- random_number_generator/lib/screen/home_screen.dart
+++ random_number_generator/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:random_number_generator/constant/color.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,43 +12,64 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: PRIMARY_COLOR,
         body: SafeArea(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text('Random Number Generator'),
-              IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.settings,
-                  ))
-            ],
-          ),
-          Expanded(
+          child: Padding(
+            //EdgeInsets.zero - 패딩 적용 x
+            //EdgeInsets.all(16.0) - 모든 부분에 padding 적용
+            //EdgeInsets.fromLTRB(16,8,4,2) - 왼,위,오,아래 순서로 각각 padding 부여
+            //EdgeInsets.only(top:16.0,bottom:16.0,left:16.0,right:16.0) - named parameter로 위치를 지정해 padding 부여
+            //EdgeInsets.symmetric(horizontal:8.0 , vertical: 16.0,) - 상하, 좌우 대칭별로 padding 부여
+            padding: EdgeInsets.symmetric(
+              horizontal: 16.0,
+            ),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text('123'),
-                Text('456'),
-                Text('789'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '랜덤숫자 생성기',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.settings,
+                        color: RED_COLOR,
+                      ),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('123'),
+                      Text('456'),
+                      Text('789'),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: RED_COLOR,
+                    ),
+                    onPressed: () {},
+                    child: Text('생성하기!'),
+                  ),
+                ),
               ],
             ),
           ),
-          SizedBox(
-            width: double.infinity,
-            child: ElevatedButton(
-              onPressed: () {
-
-              },
-              child: Text('생성하기!'),
-            ),
-          ),
-        ],
-      ),
-    ));
+        ));
   }
 }
