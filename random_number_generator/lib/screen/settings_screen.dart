48aca440eddd50d7bc892994d2862ece75535936
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-06-11 18:45:14 +0900

    slider 위젯 추가

--- random_number_generator/lib/screen/settings_screen.dart
+++ random_number_generator/lib/screen/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:random_number_generator/constant/color.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -8,12 +9,52 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  double maxNumber = 10000;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('HI'),
-      )
-    );
+        backgroundColor: PRIMARY_COLOR,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: Row(
+                    children: maxNumber.toInt()
+                        .toString()
+                        .split('')
+                        .map(
+                          (x) => Image.asset(
+                            'asset/img/$x.png',
+                            width: 50.0,
+                            height: 70.0,
+                          ),
+                        )
+                        .toList(),
+                  ),
+                ),
+                Slider(
+                  value: maxNumber,
+                  min: 10000,
+                  max: 1000000,
+                  onChanged: (double val) {
+                    setState(() {
+                      maxNumber = val;
+                    });
+                  },
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: RED_COLOR,
+                  ),
+                  child: Text('저장!'),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
