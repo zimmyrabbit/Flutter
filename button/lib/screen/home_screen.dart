a5c41352a245f1ae36cda2f9d04f28dfcc0bd316
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-06-12 21:51:34 +0900

    button styling

--- button/lib/screen/home_screen.dart
+++ button/lib/screen/home_screen.dart
@@ -18,13 +18,45 @@ class HomeScreen extends StatelessWidget {
             children: [
               ElevatedButton(
                 onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  // 메인 칼라
+                  primary: Colors.red,
+                  // 글자색과 눌렀을때 애니메이션 색
+                  onPrimary: Colors.black,
+                  // 그림자 색깔
+                  shadowColor: Colors.green,
+                  // 입체감의 높이
+                  elevation: 10.0,
+                  // 글자 스타일
+                  textStyle: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 20.0,
+                  ),
+                  // padding
+                  padding: EdgeInsets.all(32.0),
+                  // 테두리
+                  side: BorderSide(
+                    color: Colors.black,
+                    width: 4.0,
+                  ),
+                ),
                 child: Text('ElevatedButton'),
               ),
               OutlinedButton(
                 onPressed: () {},
+                style: OutlinedButton.styleFrom(
+                  // 글자, 애니메이션 효과
+                  primary: Colors.green,
+                  // 배경색
+                  backgroundColor: Colors.yellow,
+                ),
                 child: Text('OutlinedButton'),
               ),
               TextButton(
+                style: TextButton.styleFrom(
+                  primary: Colors.brown,
+                  backgroundColor: Colors.blue,
+                ),
                 onPressed: () {},
                 child: Text('TextButton'),
               ),
