710e3883f963c686dba4fcf52a56ac0df32c7901
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-06-12 23:29:56 +0900

    Named Route

--- navigation/lib/screen/route_three_screen.dart
+++ navigation/lib/screen/route_three_screen.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:navigation/layout/main_layout.dart';
 
 class RouteThreeScreen extends StatelessWidget {
   const RouteThreeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('route three'),
-      ),
+    final argument = ModalRoute.of(context)!.settings.arguments;
+
+    return MainLayout(
+      title: 'Route Three',
+      children: [
+        Text(
+          'argument : ${argument.toString()}',
+          textAlign: TextAlign.center,
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text('pop'),
+        )
+      ],
     );
   }
 }
--- navigation/lib/screen/route_two_screen.dart
+++ navigation/lib/screen/route_two_screen.dart
@@ -21,6 +21,15 @@ class RouteTwoScreen extends StatelessWidget {
           },
           child: Text('pop'),
         ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushNamed(
+              '/three',
+              arguments: 999,
+            );
+          },
+          child: Text('push named'),
+        ),
       ],
     );
   }
