cae5174027ff57513cf630aaa4dd708c7a9d1910
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-06-12 23:52:16 +0900

    pop method

--- navigation/lib/screen/home_screen.dart
+++ navigation/lib/screen/home_screen.dart
@@ -7,23 +7,47 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MainLayout(
-      title: 'Home Screen',
-      children: [
-        ElevatedButton(
-          onPressed: () async {
-            final result = await Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (BuildContext context) => RouteOneScreen(
-                  number: 123,
+    return WillPopScope( //시스템상 뒤로가기
+      onWillPop: () async {
+        // true - pop 가능
+        // false - pop 불가능
+        final canPop = Navigator.of(context).canPop();
+
+        return canPop;
+      },
+      child: MainLayout(
+        title: 'Home Screen',
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              print(Navigator.of(context).canPop());
+            },
+            child: Text('canpop'),
+          ),
+          //maybePop
+          // route stack에 뒤로가기 할 페이지가 없을떄 뒤로 안가짐
+          // canPop() 이 true 일때만
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).maybePop();
+            },
+            child: Text('maybePop'),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              final result = await Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (BuildContext context) => RouteOneScreen(
+                    number: 123,
+                  ),
                 ),
-              ),
-            );
-            print(result);
-          },
-          child: Text('Push'),
-        )
-      ],
+              );
+              print(result);
+            },
+            child: Text('Push'),
+          )
+        ],
+      ),
     );
   }
 }
