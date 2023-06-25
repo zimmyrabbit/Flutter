920a79a8575f924dcb150edb8bfb25d1cf85e3d1
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-06-25 16:59:59 +0900

    agora 6.x.ver 영상통화 앱 개발

--- video_call/lib/screen/cam_screen.dart
+++ video_call/lib/screen/cam_screen.dart
@@ -1,8 +1,36 @@
+import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-class CamScreen extends StatelessWidget {
+import '../const/agora.dart';
+
+class CamScreen extends StatefulWidget {
   const CamScreen({super.key});
+
+  @override
+  State<CamScreen> createState() => _CamScreenState();
+}
+
+class _CamScreenState extends State<CamScreen> {
+  RtcEngine? engine;
+
+  //내 ID
+  int? uid = 0;
+
+  //상대 ID
+  int? otherUid;
+
+  @override
+  void dispose() async {
+    //dispose시 채널 나가기 및 engine 폐기처리
+    if (engine != null) {
+      await engine!.leaveChannel(
+        options: LeaveChannelOptions(),
+      );
+      engine!.release();
+    }
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,26 +41,91 @@ class CamScreen extends StatelessWidget {
       body: FutureBuilder<bool>(
         future: init(),
         builder: (context, snapshot) {
-          if(snapshot.hasError) {
+          if (snapshot.hasError) {
             return Center(
-              child:Text(
+              child: Text(
                 snapshot.error.toString(),
               ),
             );
           }
 
-          if(!snapshot.hasData) {
+          if (!snapshot.hasData) {
             return Center(
               child: CircularProgressIndicator(),
             );
           }
 
           return Column(
-            children: [],
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: Stack(
+                  children: [
+                    renderMainView(),
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Container(
+                        color: Colors.grey,
+                        height: 160,
+                        width: 120,
+                        child: renderSubView(),
+                      ),
+                    ),
+                  ],
+                )
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: ElevatedButton(
+                  onPressed: () async {
+                    if (engine != null) {
+                      await engine!.leaveChannel();
+                      engine = null;
+                    }
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('채널 나가기'),
+                ),
+              ),
+            ],
           );
-        }
+        },
       ),
     );
+  }
+
+  renderMainView() {
+    if (uid == null) {
+      return Center(
+        child: Text('채널에 참여해 주세요.'),
+      );
+    } else {
+      //채널에 참여하고 있을때
+      return AgoraVideoView(
+        controller: VideoViewController(
+          rtcEngine: engine!,
+          canvas: VideoCanvas(
+            uid: 0,
+          ),
+        ),
+      );
+    }
+  }
+
+  renderSubView() {
+    if (otherUid == null) {
+      return Center(
+        child: Text('채널에 유저가 없습니다.'),
+      );
+    } else {
+      return AgoraVideoView(
+        controller: VideoViewController.remote(
+          rtcEngine: engine!,
+          canvas: VideoCanvas(uid:otherUid),
+          connection: RtcConnection(channelId: CHANNEL_NAME),
+        ),
+      );
+    }
   }
 
   Future<bool> init() async {
@@ -44,6 +137,71 @@ class CamScreen extends StatelessWidget {
     if (cameraPermission != PermissionStatus.granted ||
         microphonePermission != PermissionStatus.granted) {
       throw '카메라 또는 마이크 권한이 없습니다.';
+    }
+
+    if (engine == null) {
+      engine = createAgoraRtcEngine();
+
+      await engine!.initialize(
+        RtcEngineContext(
+          appId: APP_ID,
+        ),
+      );
+
+      engine!.registerEventHandler(
+        RtcEngineEventHandler(
+          //내가 채널에 입장했을때
+          // connection - 연결정보
+          // elapsed - 연결괸 시간(연결된지 얼마낟 됐는지)
+          onJoinChannelSuccess: (RtcConnection connection, int elapsed) {
+            print('채널에 입장했습니다. uid: ${connection.localUid}');
+            setState(() {
+              uid = connection.localUid;
+            });
+          },
+
+          //내가 채널에서 나갔을때
+          onLeaveChannel: (RtcConnection connection, RtcStats stats) {
+            print('채널 퇴장');
+            setState(() {
+              uid == null;
+            });
+          },
+
+          //상대방 유저가 들어왔을 때
+          onUserJoined: (RtcConnection connection, int remoteUid, int elapsed) {
+            print('상대가 채널에 입장했습니다. otherUid:$remoteUid');
+
+            setState(() {
+              otherUid = remoteUid;
+            });
+          },
+
+          //상대방 유저가 나갔을때
+          onUserOffline: (RtcConnection connection, int remoteUid,
+              UserOfflineReasonType reason) {
+            print('상대가 채널에서 나갔습니다. otherUid:${remoteUid}');
+
+            setState(() {
+              otherUid = null;
+            });
+          },
+        ),
+      );
+
+      // 카메라 활성화
+      await engine!.enableVideo();
+      // 카메라로 찍고있는 모습 휴대폰으로 송출
+      await engine!.startPreview();
+
+      ChannelMediaOptions options = ChannelMediaOptions();
+
+      await engine!.joinChannel(
+        token: TEMP_TOKEN,
+        channelId: CHANNEL_NAME,
+        uid: 0,
+        options: options,
+      );
     }
 
     return true;
