2adfb4d3149c0f82c179871261e0b1b687e90124
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-07-05 22:26:51 +0900

    ListView

--- scrollable_widget/lib/screen/home_screen.dart
+++ scrollable_widget/lib/screen/home_screen.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:scrollable_widget/layout/main_layout.dart';
+import 'package:scrollable_widget/screen/list_view_screen.dart';
 import 'package:scrollable_widget/screen/single_child_scroll_view_screen.dart';
 
+class ScreenModel {
+  final WidgetBuilder builder;
+  final String name;
+
+  ScreenModel({
+    required this.builder,
+    required this.name,
+  });
+}
+
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  final screens = [
+    ScreenModel(
+      builder: (_) => SingleChildScrollViewScreen(),
+      name: 'SingleChildScrollViewScreen',
+    ),
+    ScreenModel(
+      builder: (_) => ListViewScreen(),
+      name: 'ListViewScreen',
+    ),
+  ];
+
+  HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,18 +36,18 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            ElevatedButton(
+          children: screens.map(
+            (screen) => ElevatedButton(
               onPressed: () {
                 Navigator.of(context).push(
                   MaterialPageRoute(
-                    builder: (_) => SingleChildScrollViewScreen(),
+                    builder: screen.builder,
                   ),
                 );
               },
-              child: Text('SingleChildViewScroll'),
-            )
-          ],
+              child: Text(screen.name),
+            ),
+          ).toList(),
         ),
       ),
     );
---
+++ scrollable_widget/lib/screen/list_view_screen.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:scrollable_widget/const/colors.dart';
+import 'package:scrollable_widget/layout/main_layout.dart';
+
+class ListViewScreen extends StatelessWidget {
+  final List<int> numbers = List.generate(100, (index) => index);
+
+  ListViewScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MainLayout(
+      title: 'ListViewScreen',
+      body: renderSeparated(),
+    );
+  }
+
+  // 보이는 것만 그리고 중간중간 추가 할 Widget 그릴 수 있음
+  Widget renderSeparated() {
+    return ListView.separated(
+      itemCount: 100,
+      itemBuilder: (context, index) {
+        return renderContainer(
+          color: rainbowColors[index % rainbowColors.length],
+          index: index,
+        );
+      },
+      separatorBuilder: (context, index) {
+        index+=1;
+        //5개의 아이템마다 배너 보여주기
+        if (index % 5 == 0) {
+          return renderContainer(
+            color: Colors.black,
+            index: index,
+            height: 100,
+          );
+        }
+
+        return Container();
+      },
+    );
+  }
+
+  //기본 - 모두 한번에 그림
+  Widget renderDefault() {
+    return ListView(
+      children: numbers
+          .map(
+            (e) => renderContainer(
+              color: rainbowColors[e % rainbowColors.length],
+              index: e,
+            ),
+          )
+          .toList(),
+    );
+  }
+
+  // 보이는것만 그림
+  Widget renderBuilder() {
+    return ListView.builder(
+      itemCount: 100,
+      itemBuilder: (context, index) {
+        return renderContainer(
+          color: rainbowColors[index % rainbowColors.length],
+          index: index,
+        );
+      },
+    );
+  }
+
+  Widget renderContainer({
+    required Color color,
+    required int index,
+    double? height,
+  }) {
+    print(index);
+
+    return Container(
+      //height: height == null ? 300 : height,
+      height: height ?? 300,
+      color: color,
+      child: Center(
+        child: Text(
+          index.toString(),
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+            fontSize: 30,
+          ),
+        ),
+      ),
+    );
+  }
+}
