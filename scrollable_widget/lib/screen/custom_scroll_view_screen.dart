2b880f00c95d0894a5514af09d1a1282a8c71f7c
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-07-08 22:58:53 +0900

    CustomScrollView - SliverAppBar

--- scrollable_widget/lib/screen/custom_scroll_view_screen.dart
+++ scrollable_widget/lib/screen/custom_scroll_view_screen.dart
@@ -11,20 +11,44 @@ class CustomScrollViewScreen extends StatelessWidget {
     return Scaffold(
         body: CustomScrollView(
       slivers: [
-        SliverAppBar(
-          title: Text('CustomScrollViewScreen'),
-          centerTitle: true,
-        ),
+        renderSliverAppBar(),
+        renderBuilderSliverList(),
         renderSliverGridBuilder(),
+        renderBuilderSliverList(),
       ],
     ));
+  }
+
+  //AppBar
+  SliverAppBar renderSliverAppBar() {
+    return SliverAppBar(
+      //스크롤 했을떄 리스트의 중간에도 AppBar가 내려오게 할 수 있음
+      floating: true,
+      //AppBar 완전 고정
+      pinned: false,
+      // 자석 효과 (floating - true 일떄마 사용 가능)
+      snap: true,
+      // 맨위에서 한계 이상으로 스크롤 했을떄 남는공간 차지
+      stretch: true,
+      expandedHeight: 200,
+      collapsedHeight: 150,
+      flexibleSpace: FlexibleSpaceBar(
+        background: Image.asset(
+          'asset/img/test.jpg',
+          fit: BoxFit.cover,
+        ),
+        title: Text('FlexibleSpace'),
+      ),
+      title: Text('CustomScrollViewScreen'),
+      centerTitle: true,
+    );
   }
 
   //GridView.builder와 유사
   SliverGrid renderSliverGridBuilder() {
     return SliverGrid(
       delegate: SliverChildBuilderDelegate(
-            (context, index) {
+        (context, index) {
           return renderContainer(
             color: rainbowColors[index % rainbowColors.length],
             index: index,
