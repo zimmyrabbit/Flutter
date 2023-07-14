047ac863f900d66eaebba0499925aaa1c9d9be35
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-07-15 00:14:43 +0900

    CategoryCard 정리

--- dusty_dust/lib/component/main_stat.dart
+++ dusty_dust/lib/component/main_stat.dart
@@ -5,12 +5,14 @@ class MainStat extends StatelessWidget {
   final String imgPath; //아이콘 위치(경로)
   final String level; //오염 정도
   final String stat; //오염 수치
+  final double width; //너비
 
   const MainStat({
     required this.category,
     required this.imgPath,
     required this.level,
     required this.stat,
+    required this.width,
     super.key,
   });
 
@@ -20,27 +22,31 @@ class MainStat extends StatelessWidget {
       color: Colors.black,
     );
 
-    return Column(
-      children: [
-        Text(
-          category,
-          style: ts,
-        ),
-        const SizedBox(height: 8.0),
-        Image.asset(
-          imgPath,
-          width: 50.0,
-        ),
-        const SizedBox(height: 8.0),
-        Text(
-          level,
-          style: ts,
-        ),
-        Text(
-          stat,
-          style: ts,
-        ),
-      ],
+    return SizedBox(
+      width: width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            category,
+            style: ts,
+          ),
+          const SizedBox(height: 8.0),
+          Image.asset(
+            imgPath,
+            width: 50.0,
+          ),
+          const SizedBox(height: 8.0),
+          Text(
+            level,
+            style: ts,
+          ),
+          Text(
+            stat,
+            style: ts,
+          ),
+        ],
+      ),
     );
   }
 }
