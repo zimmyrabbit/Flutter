23c20c66d41c6d2071177c888952de8d31bce481
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-07-16 20:56:55 +0900

    HourlyCard 제작

--- dusty_dust/lib/component/category_card.dart
+++ dusty_dust/lib/component/category_card.dart
@@ -1,6 +1,8 @@
+import 'package:dusty_dust/component/main_card.dart';
 import 'package:flutter/material.dart';
 
 import '../const/colors.dart';
+import 'card_title.dart';
 import 'main_stat.dart';
 
 class CategoryCard extends StatelessWidget {
@@ -10,62 +12,33 @@ class CategoryCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 160,
-      child: Card(
-        margin: EdgeInsets.symmetric(horizontal: 8.0),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(16.0),
-          ),
-        ),
-        color: lightColor,
-        child: LayoutBuilder(
-          builder: (context, constraint) {
-
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: darkColor,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(16.0),
-                      topRight: Radius.circular(16.0),
-                    ),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Text(
-                      '종류별 통계',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w700,
-                      ),
-                      textAlign: TextAlign.center,
+      child: MainCard(
+        child: LayoutBuilder(builder: (context, constraint) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              CardTitle(
+                title: '종류별 통계',
+              ),
+              Expanded(
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  physics: PageScrollPhysics(),
+                  children: List.generate(
+                    20,
+                    (index) => MainStat(
+                      width: constraint.maxWidth / 3,
+                      category: '미세먼지$index',
+                      imgPath: 'asset/img/best.png',
+                      level: '최고',
+                      stat: '0㎍/㎥',
                     ),
                   ),
                 ),
-                Expanded(
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    physics: PageScrollPhysics(
-
-                    ),
-                    children: List.generate(
-                      20,
-                      (index) => MainStat(
-                        width: constraint.maxWidth/3,
-                        category: '미세먼지$index',
-                        imgPath: 'asset/img/best.png',
-                        level: '최고',
-                        stat: '0㎍/㎥',
-                      ),
-                    ),
-                  ),
-                )
-              ],
-            );
-          }
-        ),
+              )
+            ],
+          );
+        }),
       ),
     );
   }
--- dusty_dust/lib/screen/home_screen.dart
+++ dusty_dust/lib/screen/home_screen.dart
@@ -1,5 +1,7 @@
+import 'package:dusty_dust/component/card_title.dart';
 import 'package:dusty_dust/component/category_card.dart';
 import 'package:dusty_dust/component/main_app_bar.dart';
+import 'package:dusty_dust/component/main_card.dart';
 import 'package:dusty_dust/component/main_stat.dart';
 import 'package:dusty_dust/const/colors.dart';
 import 'package:flutter/material.dart';
@@ -22,6 +24,54 @@ class HomeScreen extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 CategoryCard(),
+                const SizedBox(height: 16.0),
+                MainCard(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      CardTitle(
+                        title: '시간별 미세먼지',
+                      ),
+                      Column(
+                        children: List.generate(24, (index) {
+                          final now = DateTime.now();
+                          final hour = now.hour;
+                          int currentHour = hour - index;
+
+                          if (currentHour < 0) {
+                            currentHour += 24;
+                          }
+
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 8.0,
+                              vertical: 4.0,
+                            ),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  child: Text('$currentHour시'),
+                                ),
+                                Expanded(
+                                  child: Image.asset(
+                                    'asset/img/good.png',
+                                    height: 18.0,
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Text(
+                                    '좋음',
+                                    textAlign: TextAlign.right,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        }),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           )
