c18bf9cede5a512629097aa0b429773b3e484378
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-09-07 23:02:35 +0900

    slidebar 색깔 맞춰주기 위해 구조 변경

--- dusty_dust/lib/component/main_drawer.dart
+++ dusty_dust/lib/component/main_drawer.dart
@@ -1,4 +1,3 @@
-import 'package:dusty_dust/const/colors.dart';
 import 'package:flutter/material.dart';
 
 import '../const/regions.dart';
@@ -8,10 +7,14 @@ typedef OnRegionTap = void Function(String region);
 class MainDrawer extends StatelessWidget {
   final OnRegionTap onRegionTap;
   final String selectdRegion;
+  final Color darkColor;
+  final Color lightColor;
 
   const MainDrawer({
     required this.onRegionTap,
     required this.selectdRegion,
+    required this.darkColor,
+    required this.lightColor,
     super.key,
   });
 
