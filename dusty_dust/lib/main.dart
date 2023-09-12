62294ce63a53dc2a6cc1c1af1c5c0c2def83fe4a
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-09-12 22:39:30 +0900

    Adapter 생성 및 적용

--- dusty_dust/lib/main.dart
+++ dusty_dust/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dusty_dust/model/stat_model.dart';
 import 'package:dusty_dust/screen/home_screen.dart';
 import 'package:dusty_dust/screen/test_screen.dart';
 import 'package:flutter/material.dart';
@@ -8,7 +9,15 @@ const testBox = 'test';
 void main() async {
   await Hive.initFlutter();
 
+  Hive.registerAdapter<StatModel>(StatModelAdapter());
+  Hive.registerAdapter<ItemCode>(ItemCodeAdapter());
+
   await Hive.openBox(testBox);
+
+  //ItemCode 별로 BOX 생성
+  for(ItemCode itemCode in ItemCode.values) {
+    await Hive.openBox(itemCode.name);
+  }
 
   runApp(
     MaterialApp(
